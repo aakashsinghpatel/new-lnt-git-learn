925249e9f6bf2da1cf7d6442b6461efc5c28e160
AKASH PATEL <AKASH.PATEL@lntinfotech.com>
2020-09-27 13:24:39 +0530

    second Commit

--- first.js
+++ first.js
@@ -1,3 +1,4 @@
 console.log('this is first git file');
 console.log('this is first git file--firstchange');
+console.log('this is first git file--Second change');
  
--- second.js
+++ second.js
@@ -1,1 +1,2 @@
 console.log('this is 2nd git file') 
+console.log('this is 2nd git file--first change') 
